592b4b89ef399590f4cc64eda2b3d18b99a14161
bond_anton <bond.anton@gmail.com>
2023-06-02 12:40:06 +0300

    Deepcopy

--- src/BDPotentiometer/digital_winder.py
+++ src/BDPotentiometer/digital_winder.py
@@ -228,11 +228,8 @@ class SpiDigitalWinder(DigitalWinder):
         result = cls.__new__(cls)
         memo[id(self)] = result
         for key, value in self.__dict__.items():
-            print("ITEM:", key, value)
             if "_spi" in key:
-                print("SPI DEV", key, value)
                 setattr(result, key, value)
-                # result.__dict__[key] = self.__dict__[key]
                 continue
             setattr(result, key, deepcopy(value, memo))
         return result
